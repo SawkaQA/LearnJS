066487fc87d6da4dd0aab3b2f6230e68d876c259
SawkaQA <aleksandr.sdobnov.qa@gmail.com>
2023-04-08 15:08:09 +0300

    update learnjs

---
+++ validator.js
@@ -0,0 +1,53 @@
+// let arr = ['{', ')', '[', '(', ')', ']', '}', '[', ']', ']'];
+// let str = arr.toString().replaceAll(',', ''); //= {)()}[]]
+// for (let i = 0; i < str.length - 1; i++) { // i=0, 1, 2, 3, 4, 5
+//     for (let j = i + 1; j < str.length; j++) { // j= 1, 2, 3, 4, 5, 6 
+//         if (str[i] == "(" && str[j] == ")" || str[i] == "{" && str[j] == "}" || str[i] == "[" && str[j] == "]") {
+//             console.log(' replace I' + i + ' J' + j + ' - ' + str[i] + str[j]);
+//             str = str.replace(str[j], '').replace(str[i], '');
+//             //Здесь строка стала на 2 символа короче 
+//             //str[i] после этой строки уже будет на 1 символ правее
+//             //str[j] будет на 2 символа правее, но нам все равно, т.к. из цикла j мы выходим по break
+//             i -= 1;//компенсируем сдвиг уменьшив i на 1. На следующей итерации к нему прибавят 1 и оно останется тем же, а символ под этим номером будет уже следующий.
+//             break;
+//         }
+//     }
+// }
+// console.log(str);
+
+
+
+
+function validator(arr) {
+
+    const types = {
+        '{': '}',
+        '[': ']',
+        '(': ')'
+    }
+    
+    let result = []
+    
+    let errors = {}
+    
+    for (let i = 0; i < arr.length; i++) {
+            if (Object.keys(types).includes(arr[i])) {
+                result.push(arr[i])
+                errors[i + 1] = arr[i]
+            } else if (Object.values(types).includes(arr[i])) {
+                if (types[result[result.length - 1]] === arr[i]) {
+                    result.pop()
+                    delete errors[i]
+                }
+                else {
+                    errors[i + 1] = arr[i]
+                }
+            } else {
+                console.log(`${i+1} ${arr[i]}`)
+                break;
+            }
+    }
+    return (Object.entries(errors).forEach(error => console.log(error.toString())))
+    }
+
+    console.log(validator(['{', ')', '[', '(', ')', ']', '}', '[', ']', ']']));
