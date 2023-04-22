ace705a21a95412c49b4dbf3adf24ec73d5fa446
SawkaQA <aleksandr.sdobnov.qa@gmail.com>
2023-04-22 23:21:24 +0300

    add taskjs4

--- validator.js
+++ validator.js
@@ -114,3 +114,18 @@ switch (operator) {
         console.log('Unknown operation, please enter correct operator');
     
 }
+
+
+// Создайте функцию deepCount, которая будет считать количество всех элементов в массиве, включая и вложенные массивы.
+
+// Вариант с циклом
+function deepCount(a){
+    let count = a.length;
+    for (let i=0; i<a.length; i++) if (Array.isArray(a[i])) count += deepCount(a[i]);
+    return count;
+}
+
+// Вариант с методом reduce
+function deepCount(a){
+    return a.reduce((s,e)=>s+(Array.isArray(e) ? deepCount(e) : 0),a.length);
+}
